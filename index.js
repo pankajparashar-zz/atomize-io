f42128feaa0c0d1f0a2d77eb0a6145ba97760531
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2016-03-25 14:43:27 +0530

    v2.0

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+'use strict'
+
 var app = require('express')();
 var http = require('http').Server(app);
+var request = require('request');
 var getCss = require('get-css');
-var fs = require('fs');
 var cssstats = require('cssstats');
+var purify = require('purify-css');
+var postcss = require('postcss');
+var gzipSize = require('gzip-size');
+
+/**
+ * PostCSS plugin
+ */
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('default', {
+        title: 'mywebsite.com',
+    });
 });
 
 app.get('/style.css', function(req, res){
@@ -18,61 +31,99 @@ app.get(['/apple-touch-icon.png', '/favicon.ico'], function(req, res){
 
 app.get('/*', function(req, res){
 
-    var html = '<!doctype html>\
-    <html>\
-        <head>\
-            <meta charset="utf-8">\
-            <title>Atomize IO - ' + req.url.substring(1) + '</title>\
-            <link rel="stylesheet" href="/style.css" media="screen" title="no title" charset="utf-8">\
-        </head>\
-        <body style="position: relative">\
-            <a href="https://twitter.com/intent/tweet?url=https://atomize-io.herokuapp.com&text=Atomize%20your%20website%20with%20Atomize.io&via=pankajparashar" style="position: absolute;top: 1em; right: 0" target="_blank">Share</a>\
-            <h1 style="border-bottom: 1px solid #ccc;">Atomize <span style="background: black; color: white; padding: .25em;">IO</span></h1>\
-            <p>Atomize your website to check how much you can benefit by adopting <strong>Atomic CSS!</strong>\
-            <br><br><span style="padding-left: 2em;">★ Suffix the URL in the address bar with the domain name.</span>\
-                 <br> <span style="padding-left: 2em;">★ Press ENTER to see the magic!</span>\
-            </span></p>\
-            <div class="window browser fading">\
-              <div class="header">\
-                <span class="bullet bullet-red"></span><span class="bullet bullet-yellow"></span><span class="bullet bullet-green"></span><span class="title"><span class="scheme">https://</span>atomize-io.herokuapp.com<span style="background-color: gold">'+ req.url +'</span></span>\
-              </div>\
-              <div style="padding: 1.5em;">';
-
-
     var url = 'http://' + req.url.substring(1);
+    request(url, function(error, response, body){
+        getCss(url, {timeout: 5000}).then(function(response){
 
-    getCss(url, {timeout: 5000}).then(function(response){
+            var result1 = cssstats(response.css);
+            var declarations1 = result1['declarations'];
 
-        result = cssstats(response.css);
-        var declarations = result['declarations'];
-        var unique_declarations = 0;
-        for (var prop in declarations['properties']) {
-            unique_declarations = unique_declarations + declarations.getUniquePropertyCount(prop);
-        }
+            var output1 = getAtomicCSS(response.css)
+            var unique1 = output1.uniqueID;
+            var acss1 = output1.outputCSS;
 
-        var aSize = Math.round((result.size*unique_declarations)/declarations.total);
-        var aGzipSize = Math.round((result.gzipSize*unique_declarations)/declarations.total);
+            purify(body, response.css, {minify: true}, function(output){
+                var result2 = cssstats(output);
+                var declarations2 = result2['declarations'];
 
-        html += '<table><tbody>';
-        html += '<tr><td colspan="2"><h1>' + declarations.total + '</h1><cite>No. of declarations (before)</cite></td>' + '<td colspan="2"><h1 style="color: green">' + unique_declarations + '</h1><cite>No. of declarations (after atomization)</cite></td></tr>';
-        html += '<tr><td><h1>' + result.size + '</h1><cite>size (in bytes)</cite></td><td><h1>' + result.gzipSize + '</h1><cite>gzip size (in bytes)</cite></td>';
-        html += '<td><h1>' + aSize + '</h1><cite>size (in bytes)</cite></td><td><h1>' + aGzipSize + '</h1><cite>gzip size (in bytes)</cite></td></tr>';
-        html += '<tr><td colspan="4"><div class="tip">Atomic CSS can reduce the page weight of <strong>' + req.url.substring(1) + '</strong> by ' +  Math.floor(((result.gzipSize-aGzipSize)/result.gzipSize)*100) + '%</div></td></tr>';
-        html += '</tbody></table></div></div>\
-        <footer style="border-top: 1px solid #ccc; margin-top: 5em; ">\
-          <p><span>on <a href="https://github.com/pankajparashar/atomize-io">Github</a> | via <a href="https://www.heroku.com/">Heroku</a></span>\
-              <span style="float: right">by <a href="https://pankajparashar.com">Pankaj Parashar</a></span>\
-          </p></footer>   <script>\
-        !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(\
-        arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];\
-        d.src="//d1l6p2sc9645hc.cloudfront.net/tracker.js";q.parentNode.\
-        insertBefore(d,q)}(window,document,"script","_gs");\
-        _gs("GSN-360111-P");\
-    </script></body></html>';
-        res.send(html);
+                var output2 = getAtomicCSS(output)
+                var unique2 = output2.uniqueID;
+                var acss2 = output2.outputCSS;
+
+                res.render('index', {
+                    title: req.url.substring(1),
+
+                    cc_brulesets: result1['rules']['total'],
+                    cc_bselectors: result1['selectors']['total'],
+                    cc_bdeclarations: result1['declarations']['total'],
+                    cc_bsize: (result1['size']/1024).toFixed(2),
+                    cc_bgzipped: (result1['gzipSize']/1024).toFixed(2),
+
+                    cc_arulesets: unique1,
+                    cc_aselectors: unique1,
+                    cc_adeclarations: unique1,
+                    cc_asize: (Buffer.byteLength(acss1, 'utf8')/1024).toFixed(2),
+                    cc_agzipped: (gzipSize.sync(acss1)/1024).toFixed(2),
+
+                    sc_brulesets: result2['rules']['total'],
+                    sc_bselectors: result2['selectors']['total'],
+                    sc_bdeclarations: result2['declarations']['total'],
+                    sc_bsize: (result2['size']/1024).toFixed(2),
+                    sc_bgzipped: (result2['gzipSize']/1024).toFixed(2),
+
+                    sc_arulesets: unique2,
+                    sc_aselectors: unique2,
+                    sc_adeclarations: unique2,
+                    sc_asize: (Buffer.byteLength(acss2, 'utf8')/1024).toFixed(2),
+                    sc_agzipped: (gzipSize.sync(acss2)/1024).toFixed(2),
+                });
+
+            });
+        });
     });
 });
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
+
+function getAtomicCSS(inputCSS) {
+
+    let uniqueID
+    let stylesCache
+
+    const dedupe = postcss.plugin('postcss-dedupe-css', (opts) => (css, result) => {
+      uniqueID = 0
+      stylesCache = {}
+
+      css.walkDecls(function (decl) {
+        var prop = decl.prop
+        var value = decl.value
+        var cacheKey = `${prop}:${value}`
+        if (!stylesCache[cacheKey]) {
+          stylesCache[cacheKey] = {
+            id: `__style${++uniqueID}`,
+            style: { [prop]: value }
+          }
+        }
+      })
+    })
+
+    postcss([dedupe()]).process(inputCSS).css
+
+    const createCssDeclarations = (style) => Object.keys(style).map((prop) => {
+      const value = style[prop]
+      return `${prop}:${value};`
+    }).sort().join('')
+
+    const dedupedCSS = Object.keys(stylesCache).sort().reduce((str, key) => {
+      const id = stylesCache[key].id
+      const style = stylesCache[key].style
+      const declarations = createCssDeclarations(style)
+      const rule = `\n.${id}{${declarations}}`
+      str += rule
+      return str
+    }, '')
+
+    return { uniqueID: uniqueID, outputCSS: dedupedCSS }
+}
